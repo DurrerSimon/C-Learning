f16007794d1561ea1911e698a51508c2b6785fcd
Didnt Read Readme <didnt.read.readme@example.com>
2025-04-13 14:01:34 +0200

    initial commit with exercieses

---
+++ P01_Erste_Schritte_mit_C/ChfinBTC.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define NUM_ROWS 8
+
+double convertBTCtoCHF(double chf, double rate) {
+    return chf / rate;
+}
+
+int main() {
+    double chf = 200;
+    double rate;
+
+    printf("Enter conversion rate (1.00 BTC -> CHF): ");
+    scanf("%lf", &rate);
+    double btc = convertBTCtoCHF(chf, rate);
+
+    for (int i = 1; i <= NUM_ROWS; i++) {
+        double chf = i * 200;
+        double btc = convertBTCtoCHF(chf, rate);
+        printf("%5.0f CHF\t<-->\t%8.5f BTC\n", chf, btc);
+    }
+
+    return 0;
+}
+
+
